ee52b7951f5f9ec1f12724a02c7e31147c4efe1f
Juan Pinilla <dark@shamain.com.ar>
2012-04-08 00:06:50 -0500

    test

---
+++ examples/app.js
@@ -0,0 +1,11 @@
+var og = require('../index');
+
+var OpenGraph = og.OpenGraph;
+
+var test = new OpenGraph('http://www.rottentomatoes.com/m/10011268-oceans/');
+test.get('', function(response) {
+	console.log(response);
+});
+/*test.get('title', function(response) {
+	console.log(response);
+});*/
